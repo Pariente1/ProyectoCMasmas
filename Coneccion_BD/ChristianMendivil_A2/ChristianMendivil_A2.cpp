f8af0d6fbc917836b0adf99dbe1c144f9e4508a4
Pariente <neodante12@gmail.com>
2024-11-27 10:17:12 -0700

    creamos la coleecion de Coneccion_BD, para que nos diera una salida de la tabla con sus respectivos datos, ademas agregamos una función para que sustituyera las vocales con acento por su respectiva versión sin acentos

--- Coneccion_BD/ChristianMendivil_A2/ChristianMendivil_A2.cpp
+++ Coneccion_BD/ChristianMendivil_A2/ChristianMendivil_A2.cpp
@@ -3,14 +3,31 @@
 #include <sql.h>
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
-int main()
-{
+// Función para eliminar acentos
+wstring eliminarAcentos(const wstring& input) {
+    wstring output = input;
+    for (size_t i = 0; i < output.length(); ++i) {
+        switch (output[i]) {
+        case L'á': case L'Á': output[i] = L'a'; break;
+        case L'é': case L'É': output[i] = L'e'; break;
+        case L'í': case L'Í': output[i] = L'i'; break;
+        case L'ó': case L'Ó': output[i] = L'o'; break;
+        case L'ú': case L'Ú': output[i] = L'u'; break;
+        case L'ñ': case L'Ñ': output[i] = L'n'; break;
+        default: break;
+        }
+    }
+    return output;
+}
+
+int main() {
     SQLHENV hEnv;
     SQLHDBC hDbc;
-    SQLHSTMT hStmt;  // Declaración del gestor de sentencias
+    SQLHSTMT hStmt;
     SQLRETURN ret;
 
     // Asignar un gestor de entorno
@@ -32,57 +49,62 @@ int main()
         // Cambiar a la base de datos "Escuela"
         ret = SQLExecDirect(hStmt, (SQLWCHAR*)L"USE Escuela", SQL_NTS);
 
-        if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO) {
-            // Captura errores al cambiar la base de datos
-            SQLWCHAR sqlState[6], message[256];
-            SQLINTEGER nativeError;
-            SQLSMALLINT textLength;
+        if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+            // Ejecutar la consulta en la tabla "empleados"
+            ret = SQLExecDirect(hStmt, (SQLWCHAR*)L"SELECT num_empleado, nombre, apellido_materno FROM dbo.empleados", SQL_NTS);
 
-            SQLGetDiagRec(SQL_HANDLE_STMT, hStmt, 1, sqlState, &nativeError, message, sizeof(message), &textLength);
-            wcout << L"Error al seleccionar la base de datos." << endl;
-            wcout << L"SQL State: " << sqlState << endl;
-            wcout << L"Native Error: " << nativeError << endl;
-            wcout << L"Message: " << message << endl;
-            return 1; // Salir si no se puede seleccionar la base de datos
-        }
+            if (ret == SQL_SUCCESS || ret == SQL_SUCCESS_WITH_INFO) {
+                const int columnWidth = 25;
 
-        // Ejecutar la consulta en la tabla "empleados"
-        ret = SQLExecDirect(hStmt, (SQLWCHAR*)L"SELECT num_empleado, nombre, apellido_materno FROM dbo.empleados", SQL_NTS);
+                // Imprimir línea superior de la tabla
+                wcout << L"+" << wstring(columnWidth, '-') << L"+" << wstring(columnWidth, '-') << L"+" << wstring(columnWidth, '-') << L"+" << endl;
 
-        if (ret != SQL_SUCCESS && ret != SQL_SUCCESS_WITH_INFO) {
-            // Captura errores en la consulta
-            SQLWCHAR sqlState[6], message[256];
-            SQLINTEGER nativeError;
-            SQLSMALLINT textLength;
+                // Imprimir encabezados
+                wcout << left << L"|"
+                    << setw(columnWidth) << L"num_empleado"
+                    << L"|" << setw(columnWidth) << L"nombre"
+                    << L"|" << setw(columnWidth) << L"apellido_materno" << L"|" << endl;
 
-            SQLGetDiagRec(SQL_HANDLE_STMT, hStmt, 1, sqlState, &nativeError, message, sizeof(message), &textLength);
-            wcout << L"Error al ejecutar la consulta." << endl;
-            wcout << L"SQL State: " << sqlState << endl;
-            wcout << L"Native Error: " << nativeError << endl;
-            wcout << L"Message: " << message << endl;
+                // Imprimir línea separadora
+                wcout << L"+" << wstring(columnWidth, '-') << L"+" << wstring(columnWidth, '-') << L"+" << wstring(columnWidth, '-') << L"+" << endl;
+
+                // Obtener y mostrar los resultados
+                int num_empleado;
+                SQLWCHAR name[50];
+                SQLWCHAR last_name[50];
+
+                while (SQLFetch(hStmt) == SQL_SUCCESS) {
+                    SQLGetData(hStmt, 1, SQL_C_LONG, &num_empleado, 0, NULL);
+                    SQLGetData(hStmt, 2, SQL_C_WCHAR, name, sizeof(name), NULL);
+                    SQLGetData(hStmt, 3, SQL_C_WCHAR, last_name, sizeof(last_name), NULL);
+
+                    // Convertir SQLWCHAR a wstring y eliminar acentos
+                    wstring nombre = eliminarAcentos(name);
+                    wstring apellido = eliminarAcentos(last_name);
+
+                    // Imprimir los datos en formato de tabla
+                    wcout << left << L"|"
+                        << setw(columnWidth) << num_empleado
+                        << L"|" << setw(columnWidth) << nombre
+                        << L"|" << setw(columnWidth) << apellido << L"|" << endl;
+                }
+
+                // Imprimir línea inferior de la tabla
+                wcout << L"+" << wstring(columnWidth, '-') << L"+" << wstring(columnWidth, '-') << L"+" << wstring(columnWidth, '-') << L"+" << endl;
+            }
+            else {
+                wcout << L"Error al ejecutar la consulta." << endl;
+            }
+
+            // Liberar el gestor de sentencias
+            SQLFreeHandle(SQL_HANDLE_STMT, hStmt);
         }
         else {
-            // Obtener y mostrar los resultados
-            int num_empleado;
-            SQLWCHAR name[50];
-            SQLWCHAR last_name[50];
-
-            while (SQLFetch(hStmt) == SQL_SUCCESS) {
-                SQLGetData(hStmt, 1, SQL_C_LONG, &num_empleado, 0, NULL);
-                SQLGetData(hStmt, 2, SQL_C_WCHAR, name, sizeof(name), NULL);
-                SQLGetData(hStmt, 3, SQL_C_WCHAR, last_name, sizeof(last_name), NULL);
-
-                // Imprimir los datos
-                wcout << L"Empleado #" << num_empleado << L": " << name << L" " << last_name << endl;
-
-            }
+            wcout << L"Error al seleccionar la base de datos." << endl;
         }
-
-        // Liberar el gestor de sentencias
-        SQLFreeHandle(SQL_HANDLE_STMT, hStmt);
     }
     else {
-        wcout << L"Fallo al conectar la base de datos." << endl;
+        wcout << L"Fallo al conectar a la base de datos." << endl;
     }
 
     // Desconectar y liberar manejadores
